1e23f2269930232c2e255cc9731613929de173cf
adir-ch <actiger@gmail.com>
2015-08-15 20:51:52 +1000

    Added AngularJs + Angular button menu

---
+++ adir.photography/App/AppMain.js
@@ -0,0 +1,7 @@
+﻿
+var appMainModule = angular.module('AppMain', ['mfb-button-menu']);
+
+appMainModule.controller("AppMainViewModel", function ($scope) {
+    
+});
+
---
+++ adir.photography/Scripts/mfb/js/mfb-menu.js
@@ -0,0 +1,128 @@
+var mfbButtonMenu = angular.module('mfb-button-menu', ['mfb-menu']);
+
+mfbButtonMenu.value('defaultValues', {
+  positions: [{
+    name: 'Position'
+  },{
+    value: 'tl',
+    name: 'Top left'
+  },{
+    value: 'tr',
+    name: 'Top right'
+  },{
+    value: 'br',
+    name: 'Bottom right'
+  },{
+    value: 'bl',
+    name: 'Bottom left'
+  }],
+
+  effects: [{
+    name: 'Effect'
+  },{
+    value: 'slidein',
+    name: 'Slide in + fade'
+  },{
+    value: 'zoomin',
+    name: 'Zoom in'
+  },{
+    value: 'fountain',
+    name: 'Fountain'
+  },{
+    value: 'slidein-spring',
+    name: 'Slidein (spring)'
+  }],
+
+  methods: [{
+      name: 'Method'
+    },{
+      value: 'click',
+      name: 'Click'
+    },{
+      value: 'hover',
+      name: 'Hover'
+    }
+  ],
+  actions: [{
+      name: 'Fire Main Action?'
+  }, {
+      value: 'fire',
+      name: 'Fire'
+  }, {
+      value: 'nofire',
+      name: 'Don\'t Fire'
+  }]
+});
+
+var mfbMenu = angular.module('mfb-menu', ['ng-mfb']);
+
+mfbMenu.controller('mfbMenuViewModel', Ctrl);
+
+function Ctrl(defaultValues, $window) {
+  var vm = this;
+  console.log(this);
+  vm.positions = defaultValues.positions;
+  vm.effects = defaultValues.effects;
+  vm.methods = defaultValues.methods;
+  vm.actions = defaultValues.actions;
+
+  vm.menuState = 'closed';
+  vm.loc = loc;
+  vm.setMainAction = setMainAction;
+  vm.mainAction = mainAction;
+
+  vm.chosen = {
+    effect : 'slidein',
+    position : 'br',
+    method : 'click',
+    action : 'fire'
+  };
+
+  vm.buttons = [{
+    label: 'Home',
+    icon: 'ion-ios-home',
+    href: '#index'
+  },{
+    label: 'Portfolio',
+    icon: 'ion-images',
+    href: '#portfolio-albums'
+  },{
+    label: 'About & Contact',
+    icon: 'ion-android-mail',
+    href: '#contact'
+  },{
+    label: 'Register | Login',
+    icon: 'ion-person',
+    href: '#login'
+  }];
+
+  function loc(href) {
+    $window.location.href = href;
+  }
+
+  function mainAction() {
+    //console.log('Firing Main Action!');
+  }
+
+  function setMainAction() {
+    if(vm.chosen.action === 'fire') {
+      vm.mainAction = mainAction;
+    } else {
+      vm.mainAction = null;
+    }
+  }
+}
+
+Ctrl.prototype.hovered = function() {
+  // toggle something on hover.
+};
+
+Ctrl.prototype.toggle = function() {
+  this.menuState = this.menuState === 'closed' ? 'open' : 'closed';
+};
+
+Ctrl.prototype.closeMenu = function() {
+  this.menuState = 'closed';
+};
+
+Ctrl.$inject = ['defaultValues', '$window'];
