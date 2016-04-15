6185b072c55a7a0188f80254f55fb3125bd65606
adir-ch <actiger@gmail.com>
2016-04-15 10:51:05 +1000

    Testing exception removed

--- PhotosRepository/XMLPhotoRepositoryDB.cs
+++ PhotosRepository/XMLPhotoRepositoryDB.cs
@@ -13,12 +13,12 @@ namespace PhotosRepository
         List<Photo> _photos;
         XElement _db;
         string _serverRunningPath;
-        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType); 
+        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public XMLPhotoRepositoryDB()
         {
             _serverRunningPath = System.Web.Hosting.HostingEnvironment.MapPath("~");
-            initDB(); 
+            initDB();
         }
 
         private void initDB()
@@ -29,26 +29,26 @@ namespace PhotosRepository
             }
             catch (Exception e)
             {
-                _log.ErrorFormat("Unable to load XML repo: {0}", e.Message); 
+                _log.ErrorFormat("Unable to load XML repo: {0}", e.Message);
             }
-            
-            
-            _photos = new List<Photo>(); 
+
+
+            _photos = new List<Photo>();
             var photos = _db.Element("photos").Descendants().Where(tag => tag.Name == "photo");
-            
+
             if (photos.Count() == 0)
             {
                 _log.Error("No photos were found in XML repo");
-                return; 
+                return;
             }
 
             Photo currentPhoto;
             foreach (var photo in photos)
             {
-                var captions = photo.Descendants().Where(tag => tag.Name == "caption"); 
-                currentPhoto = new Photo() 
-                { 
-                    FileName = photo.Descendants().Where(tag => tag.Name == "filename").FirstOrDefault().Value, 
+                var captions = photo.Descendants().Where(tag => tag.Name == "caption");
+                currentPhoto = new Photo()
+                {
+                    FileName = photo.Descendants().Where(tag => tag.Name == "filename").FirstOrDefault().Value,
                     Caption = (captions.Count() > 0 ? captions.FirstOrDefault().Value : "N/A")
                 };
 
@@ -56,25 +56,25 @@ namespace PhotosRepository
                 foreach (var tag in tags)
                     currentPhoto.SetTag(tag.Value);
 
-                _photos.Add(currentPhoto); 
+                _photos.Add(currentPhoto);
             }
         }
 
         private XElement GetGalleryEntry(string galleryName)
         {
-            throw new Exception(String.Format("Cannot find Gallery {0}", galleryName)); 
+            //throw new Exception(String.Format("Cannot find Gallery {0}", galleryName));  --- for testing
             return _db.Element("galleries").Descendants().Where(g => String.Equals(g.Element("name").Value, galleryName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
         }
 
         public string GetGalleryOpeningPhoto(string galleryName)
         {
-            return GetGalleryEntry(galleryName).Element("openingPhoto").Value; 
+            return GetGalleryEntry(galleryName).Element("openingPhoto").Value;
         }
 
         public IEnumerable<string> GetGalleryPhotos(string galleryName)
         {
             string openingPhoto = GetGalleryOpeningPhoto(galleryName);
-            string galleryIdentifyingTag = GetGalleryEntry(galleryName).Element("tag").Value; 
+            string galleryIdentifyingTag = GetGalleryEntry(galleryName).Element("tag").Value;
             return _photos.Where(p => p.GetTags().Where(t => t.IndexOf(galleryIdentifyingTag, StringComparison.OrdinalIgnoreCase) >= 0).Any() &&
                                         ! String.Equals(p.FileName, openingPhoto, StringComparison.CurrentCultureIgnoreCase)).Select(f => f.FileName);
         }
