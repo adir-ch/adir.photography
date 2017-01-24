f1eb1dd4f432de978b05012394404de1a82db05e
adir-ch <actiger@gmail.com>
2017-01-24 21:30:15 +1100

    Added config for relase and local testings

--- adir.photography/App/Common/Services/GlobalConfigurationService.js
+++ adir.photography/App/Common/Services/GlobalConfigurationService.js
@@ -1,37 +1,40 @@
-
-(function () {
+(function() {
     "use strict";
 
-	var GlobalSettings = {
-		ServerPath: "http://localhost:61001"
-	};
+    var GlobalSettings = {
+        /* local debug */
+        // ServerPath: "http://localhost:61001"
+
+        /* release */
+        ServerPath: "http://adir.photography"
+    };
 
     angular
-		.module("AppCommonServices")
-		.constant("GlobalSettings",GlobalSettings);
+        .module("AppCommonServices")
+        .constant("GlobalSettings", GlobalSettings);
 
 
-	angular
-		.module("AppCommonServices")
-		.factory("GlobalConfigurationService", GlobalConfigurationService);
+    angular
+        .module("AppCommonServices")
+        .factory("GlobalConfigurationService", GlobalConfigurationService);
 
-	GlobalConfigurationService.$inject = ['GlobalSettings'];
+    GlobalConfigurationService.$inject = ['GlobalSettings'];
 
-	function GlobalConfigurationService(GlobalSettings) {
+    function GlobalConfigurationService(GlobalSettings) {
 
-		var _WebApiUrls = {
-			gallery: "/api/galleryapi/",
-			galleries: "/api/galleryapi/all",
-			member: "/api/member",
-			idmLogin: "/api/idm/login",
-			idmAuthenticate: "/api/idm/login",
-			addNewInfoSubscriber: "api/registerapi/updates"
-		};
+        var _WebApiUrls = {
+            gallery: "/api/galleryapi/",
+            galleries: "/api/galleryapi/all",
+            member: "/api/member",
+            idmLogin: "/api/idm/login",
+            idmAuthenticate: "/api/idm/login",
+            addNewInfoSubscriber: "api/registerapi/updates"
+        };
 
         return {
             url: function(which) {
-            		return GlobalSettings.ServerPath + "/" + _WebApiUrls[which];
-            	}
+                return GlobalSettings.ServerPath + "/" + _WebApiUrls[which];
+            }
         };
-	}
+    }
 }());
