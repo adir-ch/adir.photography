5c1e523ce3f53b7e4fac1e5725c244a4e27a410a
adir-ch <actiger@gmail.com>
2017-01-25 19:15:44 +1100

    Updated photos DB

--- build-photos-xml.sh
+++ build-photos-xml.sh
@@ -1,1 +1,3 @@
-ls -1 | while read line ;  do     echo "<photo><filename>$line</filename><title></title><metadata><width></width><height></height></metadata><caption></caption><tags><tag></tag></tags></photo>" ; done 
+echo "<photos>"
+ls -1 *.jpg | while read line ;  do     echo "<photo><filename>$line</filename><title></title><metadata><width></width><height></height></metadata><caption></caption><tags><tag></tag></tags></photo>" ; done 
+echo "</photos>"
