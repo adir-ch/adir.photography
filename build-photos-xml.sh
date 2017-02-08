aa06a33e57e829e0e4894a27bd0254a55ffeb1b5
adir-ch <actiger@gmail.com>
2017-02-08 17:17:00 +1100

    Fixed

--- build-photos-xml.sh
+++ build-photos-xml.sh
@@ -1,3 +1,1 @@
-echo "<photos>"
-ls -1 *.jpg | while read line ;  do     echo "<photo><filename>$line</filename><title></title><metadata><width></width><height></height></metadata><caption></caption><tags><tag></tag></tags></photo>" ; done 
-echo "</photos>"
+echo "<photos>"; ls -1 *.jpg | while read line ;  do     echo "<photo><filename>$line</filename><title></title><metadata><width></width><height></height></metadata><caption></caption><tags><tag></tag></tags></photo>" ; done ; echo "</photos>"
